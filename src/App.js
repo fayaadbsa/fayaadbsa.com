dba0b39c17c19d4e769d2d6821041586679afdf6
Muhammad Fayaad <fayet256@gmail.com>
2022-04-03 06:29:49 +0700

    chat app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProjectPage from "./oldpages/ProjectPage";
 import HomePage from "./pages/HomePage";
+import CherewetPage from "./pages/CherewetPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/projects" element={<ProjectPage />} />
+        {/* <Route path="/projects" element={<ProjectPage />} /> */}
+        <Route path="/chat" element={<CherewetPage />} />
         <Route path="*" element={<HomePage />} />
       </Routes>
     </Router>
