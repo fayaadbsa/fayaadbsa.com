301dab5cbb832fc91a2a1615eeb47260dabe0b55
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-01-13 01:43:13 +0700

    remove chat app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from "react";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import CherewetPage from "./pages/CherewetPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/chat" element={<CherewetPage />} /> */}
         <Route path="*" element={<HomePage />} />
       </Routes>
     </Router>
