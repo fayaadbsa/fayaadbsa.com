b7ac7c240dd8de35dffc0080acf7362a75e77415
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-01-13 01:43:19 +0700

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center py-6">
+    <footer className="flex flex-col justify-center items-center py-6">
       <p className="text-white">
-        Made with 🧡 by{" "}
+        Developed with 🧡 by{" "}
         <span className="text-fx-linear-orange-yellow-aqua font-bold text-lg">
           Fayaad
+        </span>
+      </p>
+      <p className="text-white">
+        Designed with 💛 by{" "}
+        <span className="text-fx-linear-orange-yellow-aqua font-bold text-lg">
+          Zalfa
         </span>
       </p>
     </footer>
