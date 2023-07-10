739deceb6e7924333707fd954e2b5463e8ef439c
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-07-11 04:25:45 +0700

    fix: update vite dom

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
